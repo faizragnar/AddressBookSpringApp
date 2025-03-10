7995b52eecad6be264120f1ae7c405fa78c930c0
unknown <faiyazamail@gmail.com>
2025-03-10 22:51:21 +0530

    [Faiyaj ahmed] Add . forgot and reset password api

--- src/main/java/org/example/spring_addressbookapp/controller/AuthController.java
+++ src/main/java/org/example/spring_addressbookapp/controller/AuthController.java
@@ -7,6 +7,7 @@ import org.example.spring_addressbookapp.dto.AuthUserDTO;
 import org.example.spring_addressbookapp.dto.LoginDTO;
 import org.example.spring_addressbookapp.service.AuthenticationServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -29,6 +30,28 @@ public class AuthController {
     @Operation(summary = "User Login", description = "Allows users to log in and receive a JWT token")
     public Map<String, String> loginUser(@Valid @RequestBody LoginDTO loginDTO) {
         return authenticationServiceInterface.loginUser(loginDTO);
+    }
+    @PutMapping("/forgot/{email}")
+    @Operation(summary = "Forgot Password", description = "Allows users to reset their password if forgotten")
+    public Map<String, String> forgotPassword(@PathVariable String email, @RequestBody Map<String, String> requestBody) {
+        String responseMessage = authenticationServiceInterface.forgotPassword(email, requestBody.get("password"));
+        return Map.of("message", responseMessage);
+    }
+
+    @PutMapping("/resetPassword/{email}")
+    @Operation(summary = "Reset Password")
+    public ResponseEntity<Map<String, String>> resetPassword(
+            @PathVariable String email,@RequestBody Map<String, String> passwordRequest) {
+
+        String currentPassword = passwordRequest.get("currentPassword");
+        String newPassword = passwordRequest.get("newPassword");
+
+        if (currentPassword == null || newPassword == null) {
+            return ResponseEntity.badRequest().body(Map.of("message", "Both current and new passwords are required!"));
+        }
+
+        String responseMessage = authenticationServiceInterface.resetPassword(email, currentPassword, newPassword);
+        return ResponseEntity.ok(Map.of("message", responseMessage));
     }
 
 
